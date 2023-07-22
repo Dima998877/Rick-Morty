a7c3a2fcea14a394e2e018ca1f95530947f3fea9
Dima Karkanitsa <d.karkanitsa@gmail.com>
2023-07-22 15:16:09 +0200

    Link to episode info added

--- src/components/EpisodesItem/EpisodesItem.tsx
+++ src/components/EpisodesItem/EpisodesItem.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import styles from "./EpisodesItem.module.css"
 
 function EpisodesItem({
@@ -13,14 +14,16 @@ function EpisodesItem({
 }) {
   return (
     <div className={styles.episodes_item}>
-      <img src={coverImg} alt='episode cover' />
-      <div className={styles.text_container}>
-        <div className={styles.episode_title}>
-          <h3>{name}</h3>
+      <Link to={`/episodes/${episode}`}>
+        <img src={coverImg} alt='episode cover' />
+        <div className={styles.text_container}>
+          <div className={styles.episode_title}>
+            <h3>{name}</h3>
+          </div>
+          <p>{episode}</p>
+          <p>{date}</p>
         </div>
-        <p>{episode}</p>
-        <p>{date}</p>
-      </div>
+      </Link>
     </div>
   )
 }
